453a75ea9dd1c44d925ea02b251abb6703aacd48
Christopher Wilks <cwilks@podk.local>
2014-04-28 15:42:58 -0700

    moved all subdir py scripts into pyscripts

--- job_upload.sh
+++ job_upload.sh
@@ -2,17 +2,13 @@
 
 BASEDIR="$(cd `dirname $0`; pwd)"
 
-#PYTHON=/pod/opt/bin/python
-UPLOAD_KEY=/pod/home/cwilks/UCSC_PAWG.key
-PYTHON=python
-
 VOLUME=$1
 UUID=$2
 
 . $BASEDIR/align.conf
 
 
-BAM_DIR=$VOLUME/output/$UUID.partial
+BAM_DIR=$VOLUME/output/$UUID
 #the realigned BAM file
 BAM_FILE=$(ls $BAM_DIR/*.bam)
 #the BAM file downloaded from CGHub
@@ -34,7 +30,7 @@ if [ ! -e $SUB_DIR/PAWG.$UUID.bam ]; then
 fi
 
 #put metric compareing $ORIG_FILE and $BAM_FILE and save stats to $SUB_DIR
-$PYTHON $BASEDIR/cghub_metadata_generator/realigned_bam_check -o $ORIG_FILE -n $BAM_FILE -p $SUB_DIR
+$PYTHON $BASEDIR/pyscripts/realigned_bam_check -o $ORIG_FILE -n $BAM_FILE -p $SUB_DIR
 if [ $? != 0 ]; then
 	echo "Realignment Check error"
 	exit 1
@@ -47,24 +43,21 @@ if [ $? != 0 ]; then
 	exit 1
 fi
 
-$PYTHON $BASEDIR/synapseICGCMonitor getResultID $NORMAL_UUID 2> $SUB_DIR/new_normal_uuid
-if [ $? != 1 ]; then
+NEW_NORMAL_UUID=`$PYTHON $BASEDIR/synapseICGCMonitor getResultID $NORMAL_UUID`
+if [ $? != 0 ]; then
 	echo "Failed to get new normal uuid from Synapse error"
 	exit 1
 fi
-NEW_NORMAL_UUID=`cat $SUB_DIR/new_normal_uuid`
 
-
-$PYTHON $BASEDIR/synapseICGCMonitor getResultID $UUID 2> $SUB_DIR/new_uuid
-if [ $? != 1 ]; then
+NEW_UUID=`$PYTHON $BASEDIR/synapseICGCMonitor getResultID $UUID`
+if [ $? != 0 ]; then
 	echo "Failed to get new uuid from Synapse error"
 	exit 1
 fi
-NEW_UUID=`cat $SUB_DIR/new_uuid`
 
 #create cghub validating metadata with ICGC specific metadata added to it
 pushd $SUB_DIR
-$PYTHON $BASEDIR/cghub_metadata_generator/create_pawg_metadata -u $UUID -f PAWG.$UUID.bam -c `cat $BAM_FILE.md5` -p $SUB_DIR -t $NEW_NORMAL_UUID -n $NEW_UUID 
+$PYTHON $BASEDIR/pyscripts/create_pawg_metadata -u $UUID -f PAWG.$UUID.bam -c `cat $BAM_FILE.md5` -p $SUB_DIR -t $NEW_NORMAL_UUID -n $NEW_UUID 
 if [ $? != 0 ]; then
 	echo "CGHub metadata creation error"
 	exit 1
@@ -73,24 +66,24 @@ popd
 
 #submit to cghub (or just validate)
 pushd $SUB_DIR
-#$PYTHON $BASEDIR/cghub_metadata_generator/cgsubmit --validate-only -u $NEW_UUID
-#if [ $? != 0 ]; then
-#	echo "CGHub metadata validation error"
-#	exit 1
-#fi
+$PYTHON $BASEDIR/pyscripts/cgsubmit --validate-only -u $NEW_UUID
+if [ $? != 0 ]; then
+	echo "CGHub metadata validation error"
+	exit 1
+fi
 
 #uncomment to run for real, changes CGHub production!
-$PYTHON $BASEDIR/cghub_metadata_generator/cgsubmit -c $UPLOAD_KEY -u $NEW_UUID
-if [ $? != 0 ]; then
-	echo "CGHub metadata submission error"
-	exit 1
-fi
+#$PYTHON $BASEDIR/pyscripts/cgsubmit -c $UPLOAD_KEY -u $NEW_UUID
+#if [ $? != 0 ]; then
+#	echo "CGHub metadata submission error"
+#	exit 1
+#fi
 #upload data to cghub
-gtupload -c $UPLOAD_KEY -u $NEW_UUID/manifest.xml -vv 2>$SUB_DIR/upload.stderr.log
-if [ $? != 0 ]; then
-	echo "CGHub file upload error, check error log $SUB_DIR/upload.stderr.log"
-	exit 1
-fi
+#gtupload -c $UPLOAD_KEY -u $NEW_UUID/manifest.xml -vv 2>$SUB_DIR/upload.stderr.log
+#if [ $? != 0 ]; then
+#	echo "CGHub file upload error, check error log $SUB_DIR/upload.stderr.log"
+#	exit 1
+#fi
 popd
 
 mv $SUB_DIR $FIN_DIR
